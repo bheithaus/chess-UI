ef0c12d8f377d002625238b19b9f27117bc85cee
April+Brian <april-brian@appacademy.io>
2013-03-19 12:54:26 -0700

    initial commit

---
+++ unicode.rb
@@ -0,0 +1,32 @@
+# encoding: utf-8
+
+# /\ put this at the top of your file (THE VERY TOP)
+#disclaimer, I don't understand this :)
+
+#  http://en.wikipedia.org/wiki/Chess_symbols_in_Unicode
+
+#white
+
+white = {}
+white[:k] = "♔"
+white[:q] = "♕"
+white[:r] = "♖"
+white[:n] = "♗"
+white[:b] = "♘"
+white[:p] = "♙"
+
+##black
+
+black = {}
+black[:k] = "♔"
+black[:q] = "♕"
+black[:r] = "♖"
+black[:n] = "♗"
+black[:b] = "♘"
+black[:p] = "♙"
+
+puts "white team:"
+white.each { |key, value| puts "#{value}" }
+
+puts "black team:"
+black.each { |key, value| puts "#{value}" }
